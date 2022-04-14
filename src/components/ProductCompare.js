0ce3c001e349eebab018f4df21a2e2f93f094e73
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-14 19:04:03 +0200

    ✨ Implement comparison logic

--- src/components/ProductCompare.js
+++ src/components/ProductCompare.js
@@ -7,11 +7,52 @@ const CompareItem = () => {
 };
 
 const buildCompareList = (products, properties) => {
-    // hier das Vorgehen einer NWA nachbauen:
-    // => (1) alle Produkte property für property durchgehen
-    //    (2) den Erfüllungsgrad einer property je Produkt ermitteln
-    //    (3) jedes Produkt mit einer Summe versehen und der Summe nach Ordnen
-    //    (4) (optional) farbliche Marker für die properties einstellen, je nach Erfüllungsgrad
+    const propertiesWrapper = {};
+    const productsWrapper = {};
+    properties.filter(property => (property.visible && property.selected)).forEach(property => {
+        propertiesWrapper[property.id] = {
+            id: property.id,
+            weight: property.weight,
+            name: property.name
+        }
+    });
+    products.filter(product => product.selected).forEach(product => {
+        const props = {};
+        product.properties.forEach(property => {
+            props[property.id] = property.value;
+        });
+        productsWrapper[product.id] = {
+            id: product.id,
+            name: product.name,
+            props: props,
+            sum: 0
+        };
+    });
+    Object.values(propertiesWrapper).forEach(property => {
+        let allValues = Object.values(productsWrapper).map(product => {
+            let value = product.props[property.id];
+            return value ? value : 0;
+        });
+        allValues = [...new Set(allValues)];
+        allValues.sort();
+        Object.values(productsWrapper).forEach(product => {
+            let value = product.props[property.id];
+            let fullfillmentLevel = allValues.indexOf(value);
+            if(!fullfillmentLevel) {
+                fullfillmentLevel = 1;
+            } else {
+                fullfillmentLevel++;
+            }
+            // TODO: hier kann ich den properties eines Produktes noch CSS-Klassen auf Basis ihrer Position in der Wertung geben :)
+            //       alternativ könnte ich auch die erste grün und die letzte rot machen
+            let calculatedValue = (fullfillmentLevel * property.weight);
+            productsWrapper[product.id].sum += calculatedValue;
+        });
+    });
+    const resultList = [];
+    Object.values(productsWrapper).forEach(product => resultList.push(product));
+    resultList.sort((a, b) => b.sum - a.sum);
+    return resultList;
 }
 
 const ProductCompare = ({ products, properties }) => {
@@ -19,10 +60,13 @@ const ProductCompare = ({ products, properties }) => {
     return (
         <div className='product-compare'>
         {
-            products.filter(product => product.selected).map(product => <h3>{ product.name }</h3>)
-        }
-        {
-            properties.filter(property => (property.visible && property.selected)).map(property => <h3>{ property.name }</h3>)
+            compareList.map(product => {
+                return (
+                    <div>
+                        <span>{ product.name }</span> ⇒ <span>{ product.sum }</span>
+                    </div>
+                );
+            })
         }
         </div>
     );
