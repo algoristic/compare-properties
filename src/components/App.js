d54418a314f0d5724c2795ab4e4aa37b62316712
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-14 13:51:59 +0200

    🔥 Remove boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,7 @@ import './App.css';
 
 function App() {
     return (
-        <div className="app">
-            <header className="app-header">
-                <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <div className="app"></div>
     );
 }
 
