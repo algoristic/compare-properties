40173facc5466df73ee244becc09a43e01decb1b
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-14 13:50:37 +0200

    ✨ Create new react-app with some boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
