b1dae7ec6c5468f3772bf2f9d511caf355b08664
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-19 11:17:42 +0200

    ♻️ Refactor property selection components

--- src/components/PropertySelect.js
+++ src/components/PropertySelect.js
@@ -3,23 +3,6 @@ import SelectItem from './SelectItem'
 
 import './PropertySelect.css'
 
-const PropertyOld = ({ property, toggle, changeWeight }) => {
-    const handleChange = (event) => {
-        changeWeight(property, event.target.value)
-        event.preventDefault();
-    }
-    return (
-        <div className='property-wrapper'>
-            <div className={`selectable-property ${property.selected ? ' property-selected' : ''}`} onClick={toggle}>
-                { property.name } ({ property.unit })
-            </div>
-            <div className='property-weight'>
-                <input type='number' min='1' max='10'  value={property.weight} onChange={handleChange} />
-            </div>
-        </div>
-    );
-};
-
 const PropertyWeight = ({ property, handleChange }) => {
     return (
         <div className='property-weight'>
@@ -35,19 +18,25 @@ const PropertyName = ({ property }) => {
 };
 
 const Property = ({ property, toggle, changeWeight }) => {
-    const handleChange = (event) => {
-        changeWeight(property, event.target.value)
-        event.preventDefault();
-    }
     return (
         <div className='property-wrapper'>
-        <SelectItem selected={property.selected} onClick={toggle} style={{ flexGrow: '100' }}>
-            <PropertyName property={property} />
-        </SelectItem>
-        <PropertyWeight property={property} handleChange={handleChange} />
+            <SelectItem selected={property.selected} onClick={toggle} style={{ flexGrow: '100' }}>
+                <PropertyName property={property} />
+            </SelectItem>
+            <PropertyWeight property={property} handleChange={event => {
+                changeWeight(property, event.target.value)
+                event.preventDefault();
+            }}>
+            </PropertyWeight>
         </div>
     );
 };
+
+const Properties = ({ list, toggle, changeWeight }) => {
+    return (
+        list.filter(property => property.visible).map(property => <Property property={property} toggle={() => toggle(property)} changeWeight={changeWeight} />)
+    );
+}
 
 const PropertySelect = ({ properties, toggle, changeWeight }) => {
     return (
@@ -56,9 +45,7 @@ const PropertySelect = ({ properties, toggle, changeWeight }) => {
                 <h4 className='selectable-property-header'>Eigenschaft</h4>
                 <h4 className='property-weight-header'>Gewichtung</h4>
             </div>
-            {
-                properties.filter(property => property.visible).map(property => <Property property={property} toggle={() => toggle(property)} changeWeight={changeWeight} />)
-            }
+            <Properties list={properties} toggle={toggle} changeWeight={changeWeight} />
         </Panel>
     );
 };
