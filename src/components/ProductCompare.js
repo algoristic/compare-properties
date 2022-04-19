312efc321bc461848c219c6cd59344a7910aabb4
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-19 12:18:13 +0200

    🚧 Correct logic: actual comparison

--- src/components/ProductCompare.js
+++ src/components/ProductCompare.js
@@ -1,11 +1,5 @@
 import './ProductCompare.css'
 
-const CompareItem = () => {
-    return (
-        <>platzhalter</>
-    );
-};
-
 const buildCompareList = (products, properties) => {
     const propertiesWrapper = {};
     const productsWrapper = {};
@@ -13,13 +7,16 @@ const buildCompareList = (products, properties) => {
         propertiesWrapper[property.id] = {
             id: property.id,
             weight: property.weight,
-            name: property.name
+            name: property.name,
+            unit: property.unit
         }
     });
     products.filter(product => product.selected).forEach(product => {
         const props = {};
         product.properties.forEach(property => {
-            props[property.id] = property.value;
+            props[property.id] = {
+                value: property.value
+            };
         });
         productsWrapper[product.id] = {
             id: product.id,
@@ -30,30 +27,78 @@ const buildCompareList = (products, properties) => {
     });
     Object.values(propertiesWrapper).forEach(property => {
         let allValues = Object.values(productsWrapper).map(product => {
-            let value = product.props[property.id];
-            return value ? value : 0;
+            let productProperty = product.props[property.id];
+            return productProperty ? productProperty.value : 0;
         });
         allValues = [...new Set(allValues)];
         allValues.sort();
         Object.values(productsWrapper).forEach(product => {
-            let value = product.props[property.id];
+            const productProperty = product.props[property.id];
+            let value = productProperty ? productProperty.value : 0;
             let fullfillmentLevel = allValues.indexOf(value);
+            const isMin = (fullfillmentLevel === 0);
+            const isMax = (fullfillmentLevel === (allValues.length - 1));
             if(!fullfillmentLevel) {
                 fullfillmentLevel = 1;
             } else {
                 fullfillmentLevel++;
             }
-            // TODO: hier kann ich den properties eines Produktes noch CSS-Klassen auf Basis ihrer Position in der Wertung geben :)
-            //       alternativ könnte ich auch die erste grün und die letzte rot machen
             let calculatedValue = (fullfillmentLevel * property.weight);
             productsWrapper[product.id].sum += calculatedValue;
+            let contextualClass = 'property';
+            if(isMin) {
+                contextualClass += ' property-is-min';
+            }
+            if(isMax) {
+                contextualClass += ' property-is-max';
+            }
+            productsWrapper[product.id].props[property.id] = {
+                value: value,
+                name: property.name,
+                unit: property.unit,
+                contextualClass: contextualClass
+            };
         });
     });
     const resultList = [];
     Object.values(productsWrapper).forEach(product => resultList.push(product));
     resultList.sort((a, b) => b.sum - a.sum);
     return resultList;
-}
+};
+
+const Property = ({ value }) => {
+    return (
+        <li className={value.contextualClass}>
+            <span>{ value.name }</span>: <span>{ value.value }</span>
+            <span>{ value.unit }</span>
+        </li>
+    );
+};
+
+const Properties = ({ list }) => {
+    return (
+        <ul>
+            { Object.values(list).filter(property => property.name).map(property => <Property value={property} />) }
+        </ul>
+    );
+};
+
+const ProductName = ({ product }) => {
+    return (
+        <h4>{ product.name }</h4>
+    );
+};
+
+const Product = ({ value }) => {
+    return (
+        <div style={{
+            display: 'flex'
+        }}>
+            <ProductName product={value} />
+            <Properties list={value.props} />
+        </div>
+    );
+};
 
 const ProductCompare = ({ products, properties }) => {
     const compareList = buildCompareList(products, properties);
@@ -63,7 +108,7 @@ const ProductCompare = ({ products, properties }) => {
             compareList.map(product => {
                 return (
                     <div>
-                        <span>{ product.name }</span> ⇒ <span>{ product.sum }</span>
+                        <Product value={product} />
                     </div>
                 );
             })
